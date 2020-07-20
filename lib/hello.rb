c0a7d4f474ebed2a0800987366e35fc6aef72e99
Jason Keeling <s2jason@yahoo.com>
2020-07-20 22:31:06 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,12 +2,15 @@ def hello_t(array)
   if block_given?
     i = 0
   
-  while i < array.length
-    yield array[i]
-    i = i + 1
-  end
+    while i < array.length
+      yield array[i]
+      i = i + 1
+    end
   
-  array
+    array
+  else
+    puts "Hey! No quarter given!"
+  end
 end
 
 # call your method here!
